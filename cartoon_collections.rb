d3305e13a064e31b877cbeb66b9dd806f2ffe7f3
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-20 17:14:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find do |element|
     valid_calls.find do |call|
-      element = call
+      element == call
     end
   end
   
